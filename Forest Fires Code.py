3b7d0a74a51db871472a7de680748cb7a9ec3cc6
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2021-11-16 11:52:59 -0500

    ohmdao

--- Forest Fires Code.py
+++ Forest Fires Code.py
@@ -28,7 +28,7 @@ df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/fore
 
 #display dataframe
 df.head()
-
+print("ohm")
 # checking for missing values (no null values)
 df.isnull().sum()
 
